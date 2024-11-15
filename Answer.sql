48e2edececcf8cb74ada736962441fd1a8f537d0
viveksunder77 <67164697+viveksunder77@users.noreply.github.com>
2024-11-15 13:51:05 +0530

    Create Answer.sql

---
+++ Answer.sql
@@ -0,0 +1,11 @@
+select c.customer_id as Customer,c.age as Age,
+item_name as Item,sum(quantity) as Quantity 
+from customers c 
+inner join sales s on c.customer_id = s.customer_id
+inner join orders o on s.sales_id = o.sales_id
+inner join items i on i.item_id = o.item_id 
+where c.age between 18 and 35
+and o.quantity > 0
+group by
+
+C.customer_id,c.age,item_name
